2df160f21657c61d7efd1b1e01f468277c0b3875
anuprajvarma <anupraj1854@gmail.com>
2022-08-14 22:40:04 +0530

    create landing page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import './App.css'
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container">
+        <div className="boxes">
+          <div>
+            <input type='text' className="room_id" placeholder="Room Id" />
+          </div>
+          <div>
+            <input type='text' className="username" placeholder="Username" />
+          </div>
+          <button type='submit' className="Join_botton">Join</button>
+          <p>If you don't have invite then create <span className="new_room">new room</span></p>
+        </div>
+      </div>
+    </>
   );
 }
 
