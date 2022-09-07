869a1e2131f1a26f14f1a4a2cb610a8de8782444
anuprajvarma <anupraj1854@gmail.com>
2022-09-07 11:56:21 +0530

    update: add images in readme

---
+++ README.md
@@ -0,0 +1,19 @@
+### ***Sync_Code***
+---
+#### *This is a paint application built with MERN stack which has the functionality of login and sign up. It is integrated with MongoDB to store user info.*
+
+---
+
+#### ***Technologies used:***
+> ***Frontend***
+* ***ReactJs***
+
+> ***Backend***
+* ***NodeJs***
+* ***ExpressJs***
+* ***MongoDB***
+
+> ***Snapshot of app:*** 
+
+<img src="images/Editor.png"/>
+<img src="images/Home.png"/>
--- frontend/src/components/Editor.js
+++ frontend/src/components/Editor.js
@@ -1,14 +1,36 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import CodeMirror from '@uiw/react-codemirror'
+import { createTheme } from '@uiw/codemirror-themes';
 import { javascript } from '@codemirror/lang-javascript';
+import { tags as t } from '@lezer/highlight';
 import './Editor.css'
+
+const myTheme = createTheme({
+    dark: 'dark',
+    settings: {
+        background: '#233338',
+        //foreground: '#0d0807',
+        caret: '#AEAFAD',
+        selection: '#87cefa',
+        //selectionMatch: '#D6D6D6',
+        gutterBackground: '#233338',
+        gutterForeground: '#4DD4C',
+        gutterBorder: '#233338',
+        lineHighlight: '#233338',
+    },
+    styles: [
+        { tag: t.comment, color: '#787b80' },
+        { tag: t.definition(t.typeName), color: '#194a7b' },
+        { tag: t.typeName, color: '#194a7b' },
+        { tag: t.tagName, color: '#008a02' },
+        { tag: t.variableName, color: '#1a00db' },
+    ],
+});
 
 const Editor = () => {
 
-
-
     return (
-        <CodeMirror height="100vh" extensions={[javascript({ jsx: true })]} />
+        <CodeMirror height="100vh" theme={myTheme} extensions={[javascript({ jsx: true })]} />
     )
 }
 
